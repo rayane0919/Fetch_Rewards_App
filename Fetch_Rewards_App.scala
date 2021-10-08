0b520f76e3702537f30ce617a10df4f068abe080
rayane0919 <72228493+rayane0919@users.noreply.github.com>
2021-10-08 18:24:54 -0400

    Update Fetch_Rewards_App.scala

--- Fetch_Rewards_App.scala
+++ Fetch_Rewards_App.scala
@@ -13,15 +13,15 @@ object Main {
    Output: A tuple where index 0 is a percentage value for DIGITAL RECEIPT used and index 1 is a percentage value for non DIGITAL RECEIPT used.
 */
   def analysis1(month: Int): (Int, Int) = {
-    var df_v3: DataFrame = spark.read.csv("/content/rewards_receipts_lat_v3.csv", header=true, inferSchema=true)
-    var subset: DataFrame = df_v3.filter(split(col("RECEIPT_PURCHASE_DATE"), "-")(1) <= month)
-    var count: Int = subset.select("DIGITAL_RECEIPT").count()
-    var df_true: Int = subset.filter(("DIGITAL_RECEIPT == true"))
-    var count_true: Double = df_true.count()/count
-    var df_false: Int = subset.filter("DIGITAL_RECEIPT == false")
-    var count_false: Double = df_false.count()/count
-    var results: (Int, Int) = (count_true, count_false)
-    return results
+    var df_v3: DataFrame = spark.read.csv("/content/rewards_receipts_lat_v3.csv", header=true, inferSchema=true);
+    var subset: DataFrame = df_v3.filter(split(col("RECEIPT_PURCHASE_DATE"), "-")(1) <= month);
+    var count: Int = subset.select("DIGITAL_RECEIPT").count();
+    var df_true: Int = subset.filter(("DIGITAL_RECEIPT == true"));
+    var count_true: Double = df_true.count()/count;
+    var df_false: Int = subset.filter("DIGITAL_RECEIPT == false");
+    var count_false: Double = df_false.count()/count;
+    var results: (Int, Int) = (count_true, count_false);
+    return results;
   }
 
   /*
@@ -30,19 +30,19 @@ object Main {
    Outout: A dataframe grouped by state and ordered from highest ticket purchases to the lowest ones.
 */
   def analysis2(format: String): DataFrame = {
-     var df_v3: DataFrame = spark.read.csv("/content/rewards_receipts_lat_v3.csv", header=true, inferSchema=true)
-     var store_state: DataFrame = df_v3.groupBy("STORE_STATE").sum("RECEIPT_TOTAL").orderBy("sum(RECEIPT_TOTAL)", ascending=false).show()
+     var df_v3: DataFrame = spark.read.csv("/content/rewards_receipts_lat_v3.csv", header=true, inferSchema=true);
+     var store_state: DataFrame = df_v3.groupBy("STORE_STATE").sum("RECEIPT_TOTAL").orderBy("sum(RECEIPT_TOTAL)", ascending=false).show();
 
     if (format == "csv") {
-      return store_state
+      return store_state;
     }
     if (format == "JSON") {
-      store_state.write.format("json").mode("overwrite").save(new_storeState)
-      return new_storeState
+      store_state.write.format("json").mode("overwrite").save(new_storeState);
+      return new_storeState;
     }
     else {
-      store_state.write.format("parquet").mode("overwrite").save(new_storeState)
-      return new_storeState
+      store_state.write.format("parquet").mode("overwrite").save(new_storeState);
+      return new_storeState;
     }
   }
 
@@ -53,39 +53,39 @@ object Main {
 */
   def analysis3(): Unit = {
      //Clean the data.
-    var df_v2: DataFrame = spark.read.csv("/content/rewards_receipts_item_lat_v2.csv", header=true, inferSchema=true)
-    var df_v2: DataFrame = df_v2.na.drop(how="any", subset=("ITEM_PRICE", "WEIGHT", "PRODUCT_NAME", "CATEGORY")
+    var df_v2: DataFrame = spark.read.csv("/content/rewards_receipts_item_lat_v2.csv", header=true, inferSchema=true);
+    var df_v2: DataFrame = df_v2.na.drop(how="any", subset=("ITEM_PRICE", "WEIGHT", "PRODUCT_NAME", "CATEGORY");
     //Create feature vector.
-    var indexer: StringIndexer = new StringIndexer(inputCols=("CATEGORY", "PRODUCT_NAME"), outputCols=("CATEGORY_INDEXED", "PRODUCT_NAME_INDEXED")
-    var df: DataFrame = indexer.fit(df_v2).transform(df_v2)
-    var featureVector: VectorAssembler = new VectorAssembler(inputCols=("WEIGHT", "ITEM_PRICE", "CATEGORY_INDEXED"), outputCol="Independent Features")
-    var new_df: DataFrame = featureVector.transform(df)
+    var indexer: StringIndexer = new StringIndexer(inputCols=("CATEGORY", "PRODUCT_NAME"), outputCols=("CATEGORY_INDEXED", "PRODUCT_NAME_INDEXED");
+    var df: DataFrame = indexer.fit(df_v2).transform(df_v2);
+    var featureVector: VectorAssembler = new VectorAssembler(inputCols=("WEIGHT", "ITEM_PRICE", "CATEGORY_INDEXED"), outputCol="Independent Features");
+    var new_df: DataFrame = featureVector.transform(df);
     //create an instance of the model and train it.
-    var train_data, test_data = new_df.select(("Independent Features", "PRODUCT_NAME_INDEXED").randomSplit((0.75, 0.25)
-    var regressor: LogisticRegression = new LogisticRegression(labelCol="PRODUCT_NAME_INDEXED",featuresCol="Independent Features", maxIter=10, family="multinomial")
-    regressor = regressor.fit(train_data)
-    var results = regressor.evaluate(test_data)
-    results.predictions.show()
+    var train_data, test_data = new_df.select(("Independent Features", "PRODUCT_NAME_INDEXED").randomSplit((0.75, 0.25);
+    var regressor: LogisticRegression = new LogisticRegression(labelCol="PRODUCT_NAME_INDEXED",featuresCol="Independent Features", maxIter=10, family="multinomial");
+    regressor = regressor.fit(train_data);
+    var results = regressor.evaluate(test_data);
+    results.predictions.show();
 
   }
 
   // function that will execute the application.
   def application(analysis: String, month: Int, format: String): Unit = {
     if (analysis == "analysis1") {
-      println(analysis1(month))
+      println(analysis1(month));
     }
     if (analysis == "analysis2") {
-      println(analysis2(format))
+      println(analysis2(format));
     }
     else {
-      println(analysis3())
+      println(analysis3());
     }
   }
 
   def main(args: Array[String]): Unit = {
-    val analysis: String = "analysis1"
-    val month: Int = 2
-    val format: String = "csv"
-    println(application(analysis, month, format))
+    val analysis: String = "analysis1";
+    val month: Int = 2;
+    val format: String = "csv";
+    println(application(analysis, month, format));
   }
 }
